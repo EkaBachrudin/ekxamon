22fa6fa5a3fa29c00a5288d8ededabf68c76cb57
eka <eka.nursa@salt.co.id>
2025-10-28 11:06:47 +0700

    Fix unrender component

--- src/app/pokemon-list/page.tsx
+++ src/app/pokemon-list/page.tsx
@@ -3,14 +3,23 @@
 import PokemonList from '../../ui/components/pokemon/PokemonList';
 import { PokemonAxiosRepository } from '../../infrastructure/api/pokemon/pokemon-axios.repository';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function PokemonListPage() {
+function PokemonListPageContent() {
   const searchParams = useSearchParams();
   const pageParam = searchParams.get('page');
-  const page = 
-    typeof pageParam === 'string' && !isNaN(parseInt(pageParam)) 
-      ? Math.max(1, parseInt(pageParam)) 
+  const page =
+    typeof pageParam === 'string' && !isNaN(parseInt(pageParam))
+      ? Math.max(1, parseInt(pageParam))
       : 1;
-  
+
   return <PokemonList repository={new PokemonAxiosRepository()} page={page} />;
+}
+
+export default function PokemonListPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PokemonListPageContent />
+    </Suspense>
+  );
 }
